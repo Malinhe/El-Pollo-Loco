816e289bcb7ec24e439c74d5f208ca28ec53b502
Malaica Brunks <malaica.brunks@gmx.de>
2022-12-01 17:07:24 +0100

    added character

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,11 +1,10 @@
-class chicken {
-    x;
-    y;
+class Chicken extends MovableObject {
 
 
 
-    moveLeft() {
-        
+
+    constructor(){
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
     }
 
 }
