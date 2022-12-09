7d41ed618c0f0a477ea58a420838071d196e9de5
Malaica Brunks <malaica.brunks@gmx.de>
2022-12-09 11:38:30 +0100

    changed BG position

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -2,10 +2,10 @@ class BackgroundObject extends MovableObject {
 
     width = 720;
     height = 400;
-    constructor(imagePath, x, y) {
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
        
         this.x = x;
-        this.y = y;
+        this.y = 480 - this.height; //die Gesamthöhe des Canvas ist 480, unser Bild ist 400 hoch, also kann man die y-Koordinate einfach berechnen
     }
 }
--- models/character.class.js
+++ models/character.class.js
@@ -3,7 +3,7 @@ class Character extends MovableObject {
 
     constructor(){
         //ruft vom übergeordneten movableObject die loadImage() auf
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        super().loadImage('/img/2_character_pepe/2_walk/W-21.png');
     }
     jump() {
 
