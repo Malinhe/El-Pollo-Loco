5850a956ab0427dc0a0935382fcd16e1c8d3a36c
Malaica Brunks <malaica.brunks@gmx.de>
2022-12-10 12:53:54 +0100

    fixed imageFlip

--- models/world.class.js
+++ models/world.class.js
@@ -66,6 +66,18 @@ class World {
     //parameter mo stands for MovableObject
     //hiermit wird das Bild geladen
     addToMap(mo) {
+        //Bild spiegeln
+        if(mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
+
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if(mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
